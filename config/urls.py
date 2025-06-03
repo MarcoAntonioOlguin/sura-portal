e5a9e2742dcc61a50a90126b49fece1d61cd7312
MarcoAntonioOlguin <marcoolguin2244@gmail.com>
2025-06-02 22:40:29 -0600

    Adding paths (URLs)

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,18 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from proveedores.views import ProveedorViewSet
+from compras.views import SolicitudCompraViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r"proveedores", ProveedorViewSet, basename="proveedor")
+router.register(r"solicitudes", SolicitudCompraViewSet, basename="solicitud")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # Auth endpoints (login para obtener token)
+    path("api/auth/login/", obtain_auth_token, name="api_token_auth"),
+    # Rutas de las apps
+    path("api/", include(router.urls)),
 ]
